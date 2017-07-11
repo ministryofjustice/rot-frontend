96565aafed1462e6dc18714bc629a0c896f6c8e9
Josh Rowe <josh@s-block.com>
2017-07-11 17:19:43 +0100

    Add scss govuk toolkit and digital marketplace toolkit  - Add command for building css from sass  - Use digital market place css for pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, Button } from 'react-router-dom';
 
+import '../styles/Pagination.css';
+
 
 export const PaginationLink = ({to, label}) => {
     return (
@@ -16,17 +18,25 @@ export const Pagination = ({ prev, next, count, per = 20}) => {
   const totalPages = Math.ceil(count/per);
   const url = '/services';
   return (
-    <nav className="pagination">
-      <Link to="" className="previous">&lt;</Link>
-      {
-        [...new Array(totalPages)].map((x, i) =>
-          <PaginationLink
-            to={ url + '?page=' + (i+1) }
-            key={ i+1 }
-            label={ i+1 }/>
-        )
-      }
-      <Link to="" className="next">&gt;</Link>
+    <nav className="govuk-previous-and-next-navigation" role="navigation" aria-label="Pagination">
+      <ul className="group">
+
+          <li className="previous-page">
+            <a href="#" rel="previous" >
+              <span className="pagination-part-title">Previous page</span>
+              <span className="pagination-label">1 of {totalPages}</span>
+            </a>
+          </li>
+
+
+          <li className="next-page">
+            <a href="#" rel="next">
+              <span className="pagination-part-title">Next page</span>
+              <span className="pagination-label">3 of {totalPages}</span>
+            </a>
+          </li>
+
+      </ul>
     </nav>
   )
 };
